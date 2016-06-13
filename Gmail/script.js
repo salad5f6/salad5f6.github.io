7c3468ec559fe2b66b0f75a1bd80f36ff3efdd16
salad5f6 <vuducduong8@gmail.com>
2016-06-13 11:36:47 +0700

    l

--- Gmail/script.js
+++ Gmail/script.js
@@ -1,4 +1,8 @@
-
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
 
 // code cho forrm	
 $('#form-register').on('submit',function(){
